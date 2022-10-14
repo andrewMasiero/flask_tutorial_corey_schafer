590b176ef1181f4231d4bb118879298ae7ebefeb
andrewMasiero <andymasiero@gmail.com>
2022-10-14 15:00:10 -0500

    finished with video #2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-in powershell run
+To manually run flask in powershell run
 $env:FLASK_APP = "flaskblog.py"
+$env:FLASK_DEBUG = 1
 flask run
 
--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Andrew Masiero',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Hoe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @app.route("/")
-def hello():
-    return 'Hello world!'
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
